0a050883d6055487f1d7a1083f8a9a5b8c5d88e8
hppavilion1 <nathantronnes@gmail.com>
2015-06-18 15:00:21 -0800

    Fixed some bugs, etc.
    :)

--- Crawler.py
+++ Crawler.py
@@ -3,34 +3,42 @@ import requests
 import urlparse
 import re
 import time
+import warnings
+warnings.catch_warnings()
 
-linkRegex = r'''</?a((s+w+(s*=s*(?:".*?"|'.*?'|[^'">s]+))?)+s*|s*)/?>w+</a>'''
-linkPattern = re.compile(regexp_link)
+linkRegex = r'''/(\S+\.(com|net|org|edu|gov)(\/\S+)?)/'''
+linkPattern = re.compile(linkRegex)
 
 def extract_urls(doc):
-    return links = re.findall(pattern, html)
+    return [x[0][1:] for x in re.findall(linkPattern, doc)]
 
 read = []
 fetched = []
 tofetch = []
-baseurl = raw_input('Base URL (include trailing slash): ') #Get url to scrape
+baseurl = 'http://'+raw_input('Base URL: ').strip('/') #Get url to scrape
 
 ua = raw_input('User Agent (leave blank for default (recommended)): ') #Set user-agent
 if not ua:
-    ua = 'WalrusCrawler'
+    ua = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0'
 
 rp = robotparser.RobotFileParser()
-rp.seturl(baseurl+'robots.txt') #Get Robots.txt (for compliance purposes)
+rp.set_url(baseurl+'/'+'robots.txt') #Get Robots.txt (for compliance purposes)
 
 rp.read()
 
-tofetch = extract_urls(requests.get(baseurl))
+tofetch = extract_urls(requests.get(baseurl).text)
+
 while len(tofetch) > 0:
     for x in tofetch:
         if rp.can_fetch(ua, x):
             if not x in fetched:
                 fetched.append(x)
-                tofetch += extract_urls(requests.get(x))
+                try:
+                    tofetch += extract_urls(requests.get('http://'+x).text)
+                    print x
+                except:
+                    pass
+                tofetch = [y for y in tofetch if y != x]
         read.append(x)
             
         
